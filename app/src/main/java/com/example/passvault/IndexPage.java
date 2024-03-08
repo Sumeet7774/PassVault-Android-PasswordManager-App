58c201fc922ff00e6c29a6d83aacd929f2c4e2ad
Sumeet788 <kimjungun7777@gmail.com>
2024-03-08 22:32:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/passvault/IndexPage.java
@@ -0,0 +1,36 @@
+package com.example.passvault;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class IndexPage extends AppCompatActivity {
+    Button sign_up;
+    Button log_in;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index_page);
+
+
+        sign_up = (Button) findViewById(R.id.signup_Button);
+        log_in = (Button) findViewById(R.id.login_Button);
+
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =  new Intent(IndexPage.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
