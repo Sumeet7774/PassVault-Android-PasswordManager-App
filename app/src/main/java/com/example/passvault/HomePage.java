fe53c871f7e24c95be3d22083e3cb060fbff22bf
Sumeet788 <kimjungun7777@gmail.com>
2024-03-11 12:29:42 +0530

    made fragments for the bottom navigation bar

--- app/src/main/java/com/example/passvault/HomePage.java
+++ app/src/main/java/com/example/passvault/HomePage.java
@@ -1,6 +1,7 @@
 package com.example.passvault;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,11 +9,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class HomePage extends AppCompatActivity {
+
+    private BottomNavigationView bottom_navigation_view;
+    private FrameLayout frame_layout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        bottom_navigation_view = findViewById(R.id.bottomNavView);
+        frame_layout = findViewById(R.id.frameLayout);
+
+
     }
 }
