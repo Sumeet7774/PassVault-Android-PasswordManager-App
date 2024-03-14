80dc2939a73c62207daafff9eb7b917df3b848d5
Sumeet788 <kimjungun7777@gmail.com>
2024-03-14 11:33:39 +0530

    Removed Firebase Authentication. Maybe I will use MySQl.

--- app/src/main/java/com/example/passvault/SignUp.java
+++ app/src/main/java/com/example/passvault/SignUp.java
@@ -3,6 +3,7 @@ package com.example.passvault;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,7 +24,6 @@ public class SignUp extends AppCompatActivity {
     private ImageButton signup_back_button;
     private EditText email_edit_text, password_edit_text, confirm_password_edit_text;
 
-    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +36,6 @@ public class SignUp extends AppCompatActivity {
         password_edit_text = findViewById(R.id.password_signup_edittext);
         confirm_password_edit_text = findViewById(R.id.confirmpassword_signup_edittext);
 
-        auth = FirebaseAuth.getInstance();
 
         sign_up.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,6 +48,10 @@ public class SignUp extends AppCompatActivity {
                 {
                     Toast.makeText(SignUp.this, "Please provide your credentials", Toast.LENGTH_SHORT).show();
                 }
+                else if(!TextUtils.isEmpty(txt_email) && !Patterns.EMAIL_ADDRESS.matcher(txt_email).matches())
+                {
+                    Toast.makeText(SignUp.this, "Invalid Email", Toast.LENGTH_SHORT).show();
+                }
                 else if( txt_password.length() < 5 || txt_confirm_password.length() < 5)
                 {
                     Toast.makeText(SignUp.this, "Please ensure that your password consists of at least 5 characters.", Toast.LENGTH_LONG).show();
@@ -59,33 +62,9 @@ public class SignUp extends AppCompatActivity {
                 }
                 else
                 {
-                    registerUser(txt_email, txt_password);
+                    //registerUser(txt_email, txt_password);
                 }
             }
         });
-
     }
-
-    private void registerUser(String email, String password)
-    {
-        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(SignUp.this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful())
-                {
-                    Toast.makeText(SignUp.this, "Registration Successfull", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(SignUp.this, IndexPage.class);
-                    startActivity(intent);
-                    finish();
-                }
-                else
-                {
-                    Toast.makeText(SignUp.this, "Registration Unsuccessfull", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-    }
-
-
 }
