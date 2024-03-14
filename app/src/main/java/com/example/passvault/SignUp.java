d4bcc2d454fbb1203e73ca734db1b1991fed68a5
Sumeet788 <kimjungun7777@gmail.com>
2024-03-14 11:34:28 +0530

    Removed Firebase Authentication. Maybe I will use MySQl.

--- app/src/main/java/com/example/passvault/SignUp.java
+++ app/src/main/java/com/example/passvault/SignUp.java
@@ -13,11 +13,6 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-
 public class SignUp extends AppCompatActivity {
 
     private Button sign_up;
