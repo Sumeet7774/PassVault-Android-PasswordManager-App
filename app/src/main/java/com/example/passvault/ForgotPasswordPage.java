bacb518cc56c44df40182b6a2f14aa3c81ae0855
Sumeet788 <kimjungun7777@gmail.com>
2024-03-22 20:21:45 +0530

    made changes

--- app/src/main/java/com/example/passvault/ForgotPasswordPage.java
+++ app/src/main/java/com/example/passvault/ForgotPasswordPage.java
@@ -1,6 +1,11 @@
 package com.example.passvault;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +15,28 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ForgotPasswordPage extends AppCompatActivity {
 
+    private ImageButton forgotpassword_back_button;
+    private Button send_email;
+    private EditText email_reset_edittext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password_page);
+
+        forgotpassword_back_button = findViewById(R.id.forgotPasswordPage_backButton);
+        send_email = findViewById(R.id.emailSendLink_button);
+        email_reset_edittext = findViewById(R.id.emailReset_edittext);
+
+        forgotpassword_back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ForgotPasswordPage.this, LoginPage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
 
     }
 }
--- app/src/main/java/com/example/passvault/SignUp.java
+++ app/src/main/java/com/example/passvault/SignUp.java
@@ -17,8 +17,7 @@ public class SignUp extends AppCompatActivity {
 
     private Button sign_up;
     private ImageButton signup_back_button;
-    private EditText email_edit_text, password_edit_text, confirm_password_edit_text;
-
+    private EditText username_edit_text,email_edit_text, password_edit_text, confirm_password_edit_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +26,7 @@ public class SignUp extends AppCompatActivity {
 
         sign_up = findViewById(R.id.signupButton);
         signup_back_button = findViewById(R.id.signupPage_backButton);
+        username_edit_text = findViewById(R.id.username_signup_edittext);
         email_edit_text = findViewById(R.id.email_signup_edittext);
         password_edit_text = findViewById(R.id.password_signup_edittext);
         confirm_password_edit_text = findViewById(R.id.confirmpassword_signup_edittext);
@@ -44,11 +44,12 @@ public class SignUp extends AppCompatActivity {
         sign_up.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String txt_username = username_edit_text.getText().toString();
                 String txt_email = email_edit_text.getText().toString();
                 String txt_password = password_edit_text.getText().toString();
                 String txt_confirm_password = confirm_password_edit_text.getText().toString();
 
-                if( TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password) || TextUtils.isEmpty(txt_confirm_password))
+                if(TextUtils.isEmpty(txt_username) ||TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password) || TextUtils.isEmpty(txt_confirm_password))
                 {
                     Toast.makeText(SignUp.this, "Please provide your credentials", Toast.LENGTH_SHORT).show();
                 }
