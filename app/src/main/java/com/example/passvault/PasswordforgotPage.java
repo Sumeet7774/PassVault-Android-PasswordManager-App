ba29f584a9fb7e8a6734f7c7206f43080f96f6ae
Sumeet788 <kimjungun7777@gmail.com>
2024-03-22 23:11:56 +0530

    Made changes in the Password Forgot page. And now image button is working.

--- app/src/main/java/com/example/passvault/PasswordforgotPage.java
+++ app/src/main/java/com/example/passvault/PasswordforgotPage.java
@@ -1,6 +1,12 @@
 package com.example.passvault;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -9,15 +15,28 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class PasswordforgotPage extends AppCompatActivity {
 
+    private ImageButton forgot_password_back_button;
+    //private Button send_email;
+    //private EditText email_reset_edittext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_passwordforgot_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        forgot_password_back_button = findViewById(R.id.forgot_password_backButton);
+        //send_email = findViewById(R.id.emailSendLink_button);
+        //email_reset_edittext = findViewById(R.id.emailReset_edittext);
+
+        forgot_password_back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PasswordforgotPage.this, LoginPage.class);
+                startActivity(intent);
+                finish();
+            }
         });
+
     }
 }
