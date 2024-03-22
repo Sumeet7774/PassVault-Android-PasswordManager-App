7717720214252707a6b465cae19ff2f7faf79548
Sumeet788 <kimjungun7777@gmail.com>
2024-03-22 22:15:33 +0530

    Added forgot password page

--- app/src/main/java/com/example/passvault/LoginPage.java
+++ app/src/main/java/com/example/passvault/LoginPage.java
@@ -2,17 +2,12 @@ package com.example.passvault;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -32,7 +27,7 @@ public class LoginPage extends AppCompatActivity {
         forgot_password.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginPage.this, ForgotPasswordPage.class);
+                Intent intent = new Intent(LoginPage.this, PasswordforgotPage.class);
                 startActivity(intent);
 
             }
