60f11fcc677f9aa2f58a61628908a6ebfaa2c60e
Sumeet788 <kimjungun7777@gmail.com>
2024-03-26 21:43:50 +0530

    Added firebase connectivity. Added firebase dependency. Added url in the ApiEndpoints file.

--- app/src/main/java/com/example/passvault/ApiEndpoints.java
+++ app/src/main/java/com/example/passvault/ApiEndpoints.java
@@ -3,4 +3,5 @@ package com.example.passvault;
 public class ApiEndpoints {
     private static final String base_url = "https://pass-vault-project.000webhostapp.com/";
     public static final String register_url = base_url + "register.php";
+    public static final String login_url = base_url + "login.php";
 }
