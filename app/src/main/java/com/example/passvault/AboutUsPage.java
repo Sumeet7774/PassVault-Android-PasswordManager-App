dd3df1cd91ebce7895be1496a7d8a79df4d87f20
Sumeet7774 <sumeetkapadia7@gmail.com>
2024-06-25 12:59:14 +0530

    Changed the "successfull" spelling in all dialog boxes. And changed few things inside about us page.

--- app/src/main/java/com/example/passvault/AboutUsPage.java
+++ app/src/main/java/com/example/passvault/AboutUsPage.java
@@ -19,10 +19,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class AboutUsPage extends AppCompatActivity {
 
-    TextView member1Linkedin;
-    TextView member2Linkedin;
-    TextView member3Linkedin;
-    TextView member4Linkedin;
+    TextView ownerLinkedin;
     ImageButton about_us_backButton;
 
     @Override
@@ -32,10 +29,8 @@ public class AboutUsPage extends AppCompatActivity {
         setContentView(R.layout.activity_about_us_page);
 
         about_us_backButton = findViewById(R.id.aboutus_backButton);
-        member1Linkedin = findViewById(R.id.tarj_linkedin);
-        member2Linkedin = findViewById(R.id.rajdeep_linkedin);
-        member3Linkedin = findViewById(R.id.sumeet_linkedin);
-        member4Linkedin = findViewById(R.id.darsh_linkedin);
+        ownerLinkedin = findViewById(R.id.sumeet_linkedin);
+
 
         about_us_backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,37 +42,10 @@ public class AboutUsPage extends AppCompatActivity {
             }
         });
 
-        member1Linkedin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String linkedinProfileUrl = "https://www.linkedin.com/in/tarj-baxi-86621a29b/";
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(linkedinProfileUrl));
-                startActivity(intent);
-            }
-        });
-
-        member2Linkedin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String linkedinProfileUrl = "https://www.linkedin.com/in/rajdeep-dharajiya/";
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(linkedinProfileUrl));
-                startActivity(intent);
-            }
-        });
-
-        member3Linkedin.setOnClickListener(new View.OnClickListener() {
+        ownerLinkedin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String linkedinProfileUrl = "https://www.linkedin.com/in/sumeet-kapadia-25b700270/";
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(linkedinProfileUrl));
-                startActivity(intent);
-            }
-        });
-
-        member4Linkedin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String linkedinProfileUrl = "https://www.linkedin.com/in/darsh-panchal-b81415249/";
                 Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(linkedinProfileUrl));
                 startActivity(intent);
             }
