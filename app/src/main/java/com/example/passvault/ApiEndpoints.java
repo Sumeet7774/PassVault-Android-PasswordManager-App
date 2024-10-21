69c3daa9cd2a15b70b4bda939f582747d9ff6217
Sumeet7774 <sumeetkapadia7@gmail.com>
2024-10-21 20:49:04 +0530

    All Functionalities are working perfectly. Also added the app icon.

--- app/src/main/java/com/example/passvault/ApiEndpoints.java
+++ app/src/main/java/com/example/passvault/ApiEndpoints.java
@@ -1,7 +1,7 @@
 package com.example.passvault;
 
 public class ApiEndpoints {
-    private static final String base_url = "https://pass-vault-project.000webhostapp.com/";
+    private static final String base_url = "https://1134-2405-201-201f-609b-28eb-5ceb-92c9-4c79.ngrok-free.app/PassVaultApi/";
     public static final String register_url = base_url + "register.php";
     public static final String login_url = base_url + "login.php";
     public static final String updatePassword_url = base_url + "update_password.php";
